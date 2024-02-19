80570996aeb8389f57c28274a04eb9d062394c14
casamassa <marcelocasamassa@gmail.com>
2024-02-19 16:30:24 -0300

    Implementando e testando o metodo Save do adapter

--- adapters/db/product_test.go
+++ adapters/db/product_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/casamassa/hexagonal-fullcycle/adapters/db"
+	"github.com/casamassa/hexagonal-fullcycle/application"
 	"github.com/stretchr/testify/require"
 )
 
@@ -51,4 +52,27 @@ func TestProductDb_Get(t *testing.T) {
 	require.Equal(t, "Product Test", product.GetName())
 	require.Equal(t, 0.0, product.GetPrice())
 	require.Equal(t, "disabled", product.GetStatus())
+}
+
+func TestProductDb_Create(t *testing.T) {
+	setUp()
+	defer Db.Close()
+	productDb := db.NewProductDb(Db)
+
+	product := application.NewProduct()
+	product.Name = "Product Test"
+	product.Price = 25
+
+	productResult, err := productDb.Save(product)
+	require.Nil(t, err)
+	require.Equal(t, product.Name, productResult.GetName())
+	require.Equal(t, product.Price, productResult.GetPrice())
+	require.Equal(t, product.Status, productResult.GetStatus())
+
+	product.Status = "enabled"
+	productResult, err = productDb.Save(product)
+	require.Nil(t, err)
+	require.Equal(t, product.Name, productResult.GetName())
+	require.Equal(t, product.Price, productResult.GetPrice())
+	require.Equal(t, product.Status, productResult.GetStatus())
 }
