b8a92e106b055baa1614811fd2d924f92ed42467
casamassa <marcelocasamassa@gmail.com>
2024-02-20 10:13:12 -0300

    Implementando CLI com Cobra, criando servidor web, criandor handlers, criando getproduct

--- main.go
+++ main.go
@@ -1,18 +1,11 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"database/sql"
-
-	db2 "github.com/casamassa/hexagonal-fullcycle/adapters/db"
-	"github.com/casamassa/hexagonal-fullcycle/application"
-	_ "github.com/mattn/go-sqlite3"
-)
+import "github.com/casamassa/hexagonal-fullcycle/cmd"
 
 func main() {
-	db, _ := sql.Open("sqlite3", "db.sqlite")
-	productDbAdapter := db2.NewProductDb(db)
-	productService := application.NewProductService(productDbAdapter)
-	product, _ := productService.Create("Product Exemplo", 30)
-
-	productService.Enable(product)
+	cmd.Execute()
 }
